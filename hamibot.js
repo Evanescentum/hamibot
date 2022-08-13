a332bbd1cfa9fe4a04a36c29cbcbbf13679191e5
Evanescentum <han1531588664@outlook.com>
2022-08-13 20:56:25 +0800

    test

--- hamibot.js
+++ hamibot.js
@@ -1,10 +1,11 @@
 auto.waitFor(); // 文档地址：https://docs.hamibot.com/reference/widgetsBasedAutomation
 
+let i = 1;
 while (true) {
     let button = className("Button").text("抢单").findOne(20);
     if (button) {
-        log("抢单按钮存在");
         log(button);
         button.click();
+        toastLog("点击抢单" + i++);
     }
 }
